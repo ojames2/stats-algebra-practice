52fed385ecb63427317597eeac7a2305ae815ce0
Olivia James <ojames2@uccs.edu>
2025-10-14 18:29:28 -0600

    server.js

--- backend /server.js
+++ backend /server.js
@@ -40,6 +40,8 @@ console.log("Received attempt:", req.body);
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
